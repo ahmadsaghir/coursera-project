8dbe6cd27e663fe92078d0b211179fd8335f6d23
ahmadsaghir <ahmad.saghir.1999@gmail.com>
2024-02-16 13:28:17 +0100

    Mobile-friendly codes added to js and html files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+    var img = document.getElementById('weather-image');
+    var mobileImageUrl = 'images/clouds-3994154_1280.jpg';
+    if (window.innerWidth <= 1000) { // Change this value based on your mobile viewport width
+        img.src = mobileImageUrl;
+    }
+};
+
 // Function to show loading indicator
 function showLoadingIndicator() {
     document.getElementById('loadingIndicator').classList.remove('hidden');
@@ -28,15 +36,15 @@ fetch('city_coordinates.csv')
     });
 
 // Function to create a grid item for weather data
-function createWeatherGridItem(date, iconSrc, weatherHeader, maxTemp, minTemp) {
+function createWeatherGridItem(date, day, month, iconSrc, weatherHeader, maxTemp, minTemp) {
     // Create grid item element
     const gridItem = document.createElement('div');
-    gridItem.classList.add('bg-white', 'hover:bg-opacity-75', 'rounded-lg', 'overflow-hidden', 'shadow-md', 'transition', 'duration-300', 'transform', 'hover:-translate-y-1', 'hover:shadow-lg', 'sm:w-full', 'md:w-full', 'lg:w-full', 'xl:w-full');
-
+    gridItem.classList.add('bg-white', 'hover:bg-opacity-75', 'rounded-lg', 'overflow-hidden', 'shadow-md', 'transition', 'duration-300', 'transform', 'hover:-translate-y-1', 'hover:shadow-lg');
     // Populate grid item with weather data
     gridItem.innerHTML = `
         <div class="p-4 flex flex-col items-center">
-            <p class="text-lg font-semibold mb-3">${date}</p>
+            <h1 class="text-lg font-semibold"> ${day}</h1>
+            <p class="text-sm dark:text-gray-400 mb-3">${month} ${date.toString().slice(6, 8)}</p>
             <img src="${iconSrc}" alt="Weather Icon">
             <h2 class="mt-2 text-xl font-semibold mb-2">${weatherHeader}</h2>
             <div class="flex justify-center items-center mt-2">
@@ -74,12 +82,16 @@ function populateWeatherGrid(latitude, longitude) {
             dataseries.forEach(series => {
 
                 const date = series.date;
+                const dateStr = date.toString();
+                const datef = new Date(`${dateStr.slice(0, 4)}-${dateStr.slice(4, 6)}-${dateStr.slice(6, 8)}`)
+                const month = datef.toLocaleDateString('en-US', { month: 'short' });
+                const day = datef.toLocaleDateString('en-US', { weekday: 'long' });
                 const iconSrc = `images/${series.weather}.png`; // Assuming images are stored locally
-                const weatherHeader = series.weather;
+                const weatherHeader = series.weather.charAt(0).toUpperCase() + series.weather.slice(1);
                 const maxTemp = series.temp2m.max;
                 const minTemp = series.temp2m.min;
 
-                const gridItem = createWeatherGridItem(date, iconSrc, weatherHeader, maxTemp, minTemp);
+                const gridItem = createWeatherGridItem(date, day, month, iconSrc, weatherHeader, maxTemp, minTemp);
                 grid.appendChild(gridItem);
             });
         })
